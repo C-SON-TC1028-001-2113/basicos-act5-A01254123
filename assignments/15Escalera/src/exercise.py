2af72ef57417e671734de99b79a77f42d3ebc138
A01254123 <A01254123@itesm.mx>
2021-08-17 19:42:04 +0000

    Ejercicio escalera, CesarMtz

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,6 +1,9 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+    alturaCasa = int(input("Altura de la casa: "))
+    angulo = int(input("Angulo en grados: "))
+    largoescalera = round(alturaCasa/ math.sin(math.radians(angulo)))
+    print("Largo de la escalera: " + str(largoescalera))
 if __name__ == '__main__':
     main()
