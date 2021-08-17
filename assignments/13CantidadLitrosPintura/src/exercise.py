290c3b339be08965fca4851d87a2e6049538aa0e
A01254123 <A01254123@itesm.mx>
2021-08-17 19:49:01 +0000

    trabajo litros de pintura: Cesar Mtz

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,7 +1,9 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
-
+    area = float(input("Area a pintar en metros: "))
+    litroPorMetro = float(input("Rendimiento (m2/l): ")) 
+    litrosNecesarios = math.ceil(area/litroPorMetro)
+    print("Litros a comprar: " + str(litrosNecesarios))
 if __name__ == '__main__':
     main()
